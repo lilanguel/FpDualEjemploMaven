3f07c8bbeef8fe5d482f014fd3135b5597221453
a.los.rios.navarro <alum.adelosriosn.2021@iesalixar.org>
2022-04-20 11:40:38 +0200

    Fp Dual

---
+++ src/main/java/edu/fpdual/ejemplo/fpduaejemplomaven/Canon.java
@@ -0,0 +1,16 @@
+package edu.fpdual.ejemplo.fpduaejemplomaven;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+
+public class Canon {
+    private String letra;
+    private int frecuencia;
+
+
+}
---
+++ src/main/java/edu/fpdual/ejemplo/fpduaejemplomaven/Main.java
@@ -0,0 +1,7 @@
+package edu.fpdual.ejemplo.fpduaejemplomaven;
+
+public class Main {
+    public static void main(String[] args) {
+        new Canon("cala",3);
+    }
+}
